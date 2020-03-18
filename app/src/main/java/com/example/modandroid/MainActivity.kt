2b38cd020fe9e383b10258513e48d0d0c23de70c
sbenezette <sbenezette@gmail.com>
2020-03-18 14:35:57 -0400

    commit two

---
+++ app/src/main/java/com/example/modandroid/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.modandroid
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+//the initial main activity is generated here upon launch of a new app
+//fragment_notifications.xml and activity_main.xml contain the 4
+//required modifications for the purposes of this assignment
+//any other modifications have been properly labeled
+//PROJECT HAS BEEN SET UP WITH GIT HUB (modification for the assignment)
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+
+//        git repository
+    }
+}
